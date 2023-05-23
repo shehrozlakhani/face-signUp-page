3e16603afa3ca4c23f8112e3e73a742c7bc12740
shehrozlakhani <128062231+shehrozlakhani@users.noreply.github.com>
2023-05-23 09:53:17 -0700

    Add files via upload

---
+++ facebook/face.js
@@ -0,0 +1,15 @@
+var firstName = document.getElementById("fname");
+var surName = document.getElementById("sname");
+var phoneNumber = document.getElementById("phnnumber");
+var pss = document.getElementById("password");
+
+
+function signup ()
+{
+
+    console.log(firstName.value);
+    console.log(surName.value);
+    console.log(phoneNumber.value);
+    console.log(pss.value);
+
+}
